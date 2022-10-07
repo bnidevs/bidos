27b1f7962c00de526b53417db84e0f9b24e1d9c3
Bill Ni <bill.8ni@gmail.com>
2022-10-07 12:28:22 -0400

    source bash profile

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install 16.17.1
-npm install pm2@latest -g
+source /home/ec2-user/.bash_profile
 export PM2_HOME=/home/ec2-user/.pm2
 cd /home/ec2-user/backend/
 pm2 start --name backend npm -- start
