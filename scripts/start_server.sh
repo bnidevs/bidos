3979923ee42e99c6fa0932c3ed54d8da833a8ed3
Bill Ni <bill.8ni@gmail.com>
2022-10-07 03:01:54 -0400

    add nvm instructions

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 16.17.1
+npm install pm2@latest -g
 export PM2_HOME=/home/ec2-user/.pm2
 cd /home/ec2-user/backend/
 pm2 start --name backend npm -- start
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 16.17.1
+npm install pm2@latest -g
 export PM2_HOME=/home/ec2-user/.pm2
 pm2 stop backend
