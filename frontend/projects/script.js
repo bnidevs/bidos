c14d5c74cb0719e40cfff9b8e7aa2ba52748e4ff
nashwanhabboosh <nashwan.habboosh@icloud.com>
2022-10-26 23:03:49 -0400

    began work on projects page

---
+++ frontend/projects/script.js
@@ -0,0 +1,20 @@
+// fetch("test.json")
+// .then(function(response){
+// 	   return response.json();
+// 	})
+// 	.then(function(projects){
+//         document.getElementById("page").innerHTML = `Projects`
+// 	   let temp = document.querySelector("#page");
+// 	   let output = "";
+// 	//    for(let product of products){
+// 	//       output += `test test`;
+// 	//    }
+//     temp.innerHTML = `testing`;
+// 	//    temp.innerHTML = output;
+// 	});
+
+fetch
+
+
+
+document.getElementById("page").innerHTML = `${2+2}`
