0a1032b37d3a4ac8a9df04107475d1afd7aeb041
IsaacEf <isaacefrosman3@gmail.com>
2022-11-11 16:05:23 -0500

    Update and rename index.js to login.js

--- frontend/dev/src/login/login.js
+++ frontend/dev/src/login/login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './login.css';
 import {FooterLink, Container, LoginButton} from './App';
 import reportWebVitals from './reportWebVitals';
 
