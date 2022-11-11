2e285d46ceb50002d6266baa931017e83e7d01f1
IsaacEf <isaacefrosman3@gmail.com>
2022-11-11 16:17:45 -0500

    Add files via upload

---
+++ frontend/dev/src/Login.js
@@ -0,0 +1,47 @@
+import './Login.css';
+
+function FooterLink(props){
+  return(
+    
+    <a href={props.linkPath} className="text">{props.displayString}</a> 
+           
+  );
+}
+
+function LoginButton(props){
+
+  return(
+
+    <div className = "input-field button">
+          <button className = "test"><i className ={props.iconName} ></i> {props.buttonText}</button>
+    </div>
+    
+  )
+}
+
+
+function Container(){
+  return(
+
+      <div className="container">
+        <div className="forms">
+          <div className="form login">
+            <form action = "#">
+              <LoginButton
+                iconName = "fa fa-github fa-lg"  
+                buttonText = "Login with GitHub"
+              />
+              <FooterLink displayString="Terms of Service" linkPath="#" />
+              <FooterLink displayString="Privacy Policy" linkPath="#" />
+            </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export {FooterLink, Container, LoginButton};
+
+
+
--- frontend/dev/src/index.js
+++ frontend/dev/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {FooterLink, Container, LoginButton} from './login/Login';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Container />
+    
+    
   </React.StrictMode>
 );
 
