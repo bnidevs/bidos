b65b6f1461cb4ac358d9530d1bb5f7c1dff5f520
Simon-Sandrew <sandrewsimon@gmail.com>
2022-11-11 21:58:00 -0500

    temp commit to desktoip

--- lambdaFunctions/index.js
+++ lambdaFunctions/index.js
@@ -1,24 +1,26 @@
-const { createOAuthUserAuth } = require("@octokit/auth-oauth-user");
 const AWS = require('aws-sdk');
-const require('crypto')
+const gen = require('crypto');
+const { Octokit } = require("@octokit/core");
+const { createOAuthUserAuth } = require("@octokit/auth-oauth-user");
+
 
 async function a(event, context, callback){
-    // TODO implement
-    //verify a user auth code
-    
-    const auth = createOAuthUserAuth({
-        clientId: "41aaee58d523ecf12369",
-        clientSecret: "*", //auth code here 
-        code: "d4fb63a1c326e7c33768" //code that was response from code
-    });
-      
-      // Exchanges the code for the user access token authentication on first call
-      // and caches the authentication for successive calls
-    const { token } = await auth();
 
-    //octokit request to get user info
-    const octokit = new Octokit({ auth: token });
+
+    const octokit = new Octokit({
+        authStrategy: createOAuthUserAuth,
+        auth:{
+            clientId: "41aaee58d523ecf12369",
+            clientSecret: "*", //auth code here 
+            code: "af56cf865862c35ecf11" //code that was response from code
+        }
+    });
+
+    // Exchanges the code for the user access token authentication on first call
+    // and caches the authentication for successive calls
     const { data } = await octokit.request("GET /user");
+
+
 
     //insert user into db
     const db = new AWS.DynamoDB({apiVersion: '2012-08-10'});
@@ -56,7 +58,7 @@ async function a(event, context, callback){
     //add user to the users tabel
     let resp = await db.putItem(params).promise();
     
-    console.log(resp);
+    // console.log(resp);
     
 };
 
