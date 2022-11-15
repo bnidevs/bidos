3e1fa86460cebad676e73b4d314719a9a1de2903
Simon Sandrew <simon@simonsandrew.com>
2022-11-14 23:01:53 -0500

    oauth flow
    working accepting post request - need to add the ability to search by github id before adding user in function

--- backend/routes/loginUser.js
+++ backend/routes/loginUser.js
@@ -1,63 +1,156 @@
-const express = require('express');
-const router = express.Router();
 const AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
-const lambda = new AWS.Lambda();
-const fetch = require( 'node-fetch' );
-
+const gen = require('crypto');
+const { Octokit } = require("@octokit/core");
+const { createOAuthUserAuth } = require("@octokit/auth-oauth-user");
+const {SecretsManagerClient,GetSecretValueCommand,}  = require("@aws-sdk/client-secrets-manager");
+const express = require('express');
+const router = express.Router();
 
 
 /* loginUser page*/
-router.get('/', async function(req, res, next) {
-   //reroute to the user login to lambda function to hide user storing
-    //781dd8dba73bcb22af21
+router.post('/', async function(req, res, next) {
 
+    const params = await req.body;
+    const secretName = "Bidos_Github_Auth_Creds";
 
-// Compare: https://docs.github.com/en/rest/reference/users#get-the-authenticated-user 
-    //get request to user 
-    
-    const temp = await fetch('https://api.github.com/user', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer 781dd8dba73bcb22af21'
-        }
+    const client = new SecretsManagerClient({
+        region: "us-east-1",
     });
 
-    const data = await temp.json();
-    console.log(data);
+    const db = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+    
+    //can prob separate into two diff functions
+    if(params.type === 'oauth'){
+        let response;
+        try {
+            response = await client.send(
+                new GetSecretValueCommand({
+                SecretId: secretName,
+                VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+            })
+        );
+        } catch (error) {
+            throw error;
+        }
+     
+        const creds = JSON.parse(response.SecretString);
+
+        const octokit = new Octokit({
+            authStrategy: createOAuthUserAuth,
+            auth:{
+                clientId: creds.clientId,
+                clientSecret: creds.clientSecret, //auth code here 
+                code: req.body.code //code that was response from code
+            }
+        });
+
+     
+        //get token from octokit
+        const token = await octokit.auth();
+     
+        // // Exchanges the code for the user access token authentication on first call
+        // // and caches the authentication for successive calls
+        const { data } = await octokit.request("GET /user");
+
+     
+
+        // this will have to be finished another time
+        // const params = {
+        //     TableName: 'bidos-users',
+        //     IndexName: "githubId-index",
+        //     KeyConditionExpression: "githubId = :ghid",
+        //     ExpressionAttributeValues: {
+        //         ":ghid": data.id.toString()
+        //     },
+        // };
+
+        //  await db.query(params, (err, res)=>{
+        //     if(err){
+        //         console.log(err);
+        //     }else{
+        //         console.log(res);
+        //     }
+        // });
     
 
-    if(resp.status !== 200){
-        res.status(401).send("Invalid token");
-    }else{
-        const payload = {
-            "auth": req.body.token,
-            "userId": resp.id
-        }
-        const params = {
-            FunctionName: 'insertNewUser',
-            InvocationType: 'RequestResponse',
-            LogType: 'Tail',
-            Payload: JSON.stringify(payload)
-        };
-    
-        lambda.invoke(params, function(err, data) {
-            if (err) {
-                console.log(err, err.stack);
-            } else {
-                console.log(data);
-                res.json({
-                    'loginUser': {
-                        title: 'Login User',
-                        loginUser: data,
-                        success: true //eventually data.success for quick access.
+        const parsedUserData = AWS.DynamoDB.Converter.marshall(data);
+        // //insert user into db
+        
+        const userUUID = await gen.randomUUID();
+        const sessionUUID = await gen.randomUUID(); 
+     
+     
+        const userParams = {
+            "TableName": "bidos-users",
+            "Item":{
+                "id": {"M": userUUID},
+                "githubId": {"S": data.id.toString()},
+                "auth":{
+                    "M": {
+                        "github": {
+                            "M": {
+                                "token": {"S": token.token},
+                                "sessionId": {"S": sessionUUID},
+                            }
+                        }
                     }
-                });
+                },
+                "user": {
+                    "M": {
+                        "creationTimestamp": {"S": (new Date).toISOString()},
+                        "userDetails": {
+                            "M": parsedUserData
+                        }
+                    }       
+                }
+            },
+            "ConditionExpression": 'attribute_not_exists(id)'
+        };
+        //epoch 2 days from now
+        const expire = Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 2);
+        const sessionParams = {
+            "TableName": "bidos-user-sessions",
+            "Item":{
+                "id" : {"S": sessionUUID},
+                "expires": {"N": expire.toString()},
             }
         }
-        );
+
+        
+
+        let userResp = await db.putItem(userParams).promise();
+        let sessionResp = await db.putItem(sessionParams).promise();
+
+        res.send({
+            "status": "success",
+            "session": sessionUUID
+        });
+        console.log(userResp);
+        console.logsz
+        
+    }else if(params.type === 'token'){
+        //search token in session DB
+        //if token exists and epoch is below current time return session id
+        //else return error
+
+        const sessionParams = {
+            "TableName": "bidos-user-sessions",
+            "Key":{
+                "id" : {"S": req.code}
+            }
+        }
+
+        let sessionResp = await db.getItem(sessionParams).promise();
+        if(sessionResp.Item?.expires.N < Math.floor(Date.now() / 1000)){
+        //return error
+        res.send({"status": "error", "message": "session expired"});
+        }else{
+        //return session id
+        res.send({"status": "success", "sessionId": params.code});
+        }
     }
+
 });
 
 module.exports = router;
--- lambdaFunctions/index.js
+++ lambdaFunctions/index.js
@@ -1,42 +1,72 @@
 const AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
 const gen = require('crypto');
 const { Octokit } = require("@octokit/core");
 const { createOAuthUserAuth } = require("@octokit/auth-oauth-user");
+const {SecretsManagerClient,GetSecretValueCommand,}  = require("@aws-sdk/client-secrets-manager");
 
 
 async function a(event, context, callback){
 
+    const secretName = "Bidos_Github_Auth_Creds";
+
+    const client = new SecretsManagerClient({
+        region: "us-east-1",
+    });
+
+    let response;
+
+    try {
+        response = await client.send(
+            new GetSecretValueCommand({
+            SecretId: secretName,
+            VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+        })
+    );
+    } catch (error) {
+        throw error;
+    }
+
+    const creds = JSON.parse(response.SecretString);
+    console.log(creds)
 
     const octokit = new Octokit({
         authStrategy: createOAuthUserAuth,
         auth:{
-            clientId: "41aaee58d523ecf12369",
-            clientSecret: "*", //auth code here 
-            code: "af56cf865862c35ecf11" //code that was response from code
+            clientId: creds.clientId,
+            clientSecret: creds.clientSecret, //auth code here 
+            code: "7d0d9955b16e62cc4ad6" //code that was response from code
         }
     });
+    //get token from octokit
+    const token = await octokit.auth();
 
-    // Exchanges the code for the user access token authentication on first call
-    // and caches the authentication for successive calls
+
+    
+
+    // // Exchanges the code for the user access token authentication on first call
+    // // and caches the authentication for successive calls
     const { data } = await octokit.request("GET /user");
 
+    const parsedUserData = AWS.DynamoDB.Converter.marshall(data);
 
-
-    //insert user into db
+    // //insert user into db
     const db = new AWS.DynamoDB({apiVersion: '2012-08-10'});
     
-    const uuid = await gen.randomUUID();
+    const userUUID = await gen.randomUUID();
+    const sessionUUID = await  gen.randomUUID(); 
+
 
     const params = {
         "TableName": "bidos-users",
         "Item":{
-            "id": {"S": uuid},
+            "id": {"S": userUUID},
             "auth":{
                 "M": {
                     "github": {
                         "M": {
-                            "userId":{ "S": event.userId},
-                            "auth": {"S":event.auth}
+                            "token": {"S": token.token},
+                            "sessionId": {"S": sessionUUID},
                         }
                     }
                 }
@@ -45,7 +75,7 @@ async function a(event, context, callback){
                 "M": {
                     "creationTimestamp": {"S": (new Date).toISOString()},
                     "userDetails": {
-                        "M": data
+                        "M": parsedUserData
                     }
                 }       
             }
@@ -58,7 +88,7 @@ async function a(event, context, callback){
     //add user to the users tabel
     let resp = await db.putItem(params).promise();
     
-    // console.log(resp);
+    console.log(resp);
     
 };
 
