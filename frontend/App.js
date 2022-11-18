426c2f86c19dddbbfaa9be10b399be928a95c5b9
IsaacEf <isaacefrosman3@gmail.com>
2022-11-18 16:48:34 -0500

    Linked pages together

---
+++ frontend/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { Container } from './login/Login';
+import { Page } from './landing/Landing';
+import './login/Login.css';
+import './landing/Landing.css';
+
+function App() {
+    const paths = {
+        '/login': <Container />,
+        '/': <Page />
+    };
+
+    let [component, setComponent] = useState(null);
+
+    useEffect(() => {
+        const path = window.location.pathname;
+        
+        if(path in paths){
+            setComponent(paths[path]);
+        }else{
+            setComponent(<Page />);
+        }
+    });
+
+    return (<>{component}</>);
+}
+
+export default App;
--- frontend/Landing.js
+++ frontend/Landing.js
@@ -1,6 +1,6 @@
 import './Landing.css';
 import logo from './Untitled_drawing_6.png';
-import {Link} from 'react-router-dom';
+
 
 function NavBar(props){
     return(
@@ -20,29 +20,27 @@ function NavBar(props){
 
 function Page(){
     return(
-        <body>
-            <section>
-                <header>
-                <a href = "#" ><img src = {logo} class = "logo" /></a>
-                   <ul>
-                   
-                       <NavBar displayString="Home" linkPath="#"/>
-                       <NavBar displayString="Projects" linkPath="#"/>
-                       <NavBar displayString="About" linkPath="#"/>
-                       <NavBar displayString="Contact" linkPath="#"/>
-                       <NavButton as={Link} to="/login" displayString="Login" linkPath="/login"/>
-                   
-                   </ul>
-                </header>   
-                <div class = "content">
-                    <div class = "textBox">
-                        <h2>Welcome to <span>bidOS</span></h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
-                    </div>
-                </div>                 
-            </section>
-        </body>
+        <section className='landing_main'>
+            <header>
+            <a href = "#" ><img src = {logo} class = "logo" /></a>
+                <ul>
+                
+                    <NavBar displayString="Home" linkPath="#"/>
+                    <NavBar displayString="Projects" linkPath="#"/>
+                    <NavBar displayString="About" linkPath="#"/>
+                    <NavBar displayString="Contact" linkPath="#"/>
+                    <NavButton displayString="Login" linkPath="/login"/>
+                
+                </ul>
+            </header>   
+            <div class = "content">
+                <div class = "textBox">
+                    <h2>Welcome to <span>bidOS</span></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+            </div>                 
+        </section>
 
     );
 }
