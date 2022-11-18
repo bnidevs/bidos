f327690534253682e6ce09c44fa5f8845fa9b762
IsaacEf <isaacefrosman3@gmail.com>
2022-11-18 16:49:24 -0500

    Add files via upload

--- frontend/index.js
+++ frontend/index.js
@@ -1,31 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {FooterLink, Container, LoginButton} from './login/Login';
-import {Page} from './landing/Landing';
+import {Container} from './login/Login';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Page/>
-
-    <Router>
-
-      <Routes>
-        <Route exact path="/" component={withRouter(<Page/>)} />
-        <Route path="/login" component={withRouter(<Container/>)} />
-      </Routes>
-
-
-    </Router>
-    
-    
+    <App />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
