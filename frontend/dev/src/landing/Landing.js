5cc6aca5d683eaa144579370444ad3244a2b6195
IsaacEf <isaacefrosman3@gmail.com>
2022-11-20 16:42:29 -0500

    Add files via upload

---
+++ frontend/dev/src/landing/Landing.js
@@ -0,0 +1,48 @@
+import './Landing.css';
+import logo from './Untitled_drawing_6.png';
+
+
+function NavBar(props){
+    return(
+      
+      <li><a href={props.linkPath} className="text">{props.displayString}</a></li>
+             
+    );
+  }
+
+  function NavButton(props){
+    return(
+      
+      <li><a href={props.linkPath} className="login">{props.displayString}</a></li>
+             
+      );
+  }
+
+function Page(){
+    return(
+        <section className='landing_main'>
+            <header>
+            <a href = "#" ><img src = {logo} class = "logo" /></a>
+                <ul>
+                
+                    <NavBar displayString="Home" linkPath="Home"/>
+                    <NavBar displayString="Projects" linkPath="Projects"/>
+                    <NavBar displayString="About" linkPath="About"/>
+                    <NavBar displayString="Contact" linkPath="Contact"/>
+                    <NavButton displayString="Login" linkPath="/login"/>
+                
+                </ul>
+            </header>   
+            <div class = "content">
+                <div class = "textBox">
+                    <h2>Welcome to <span>bidOS</span></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+            </div>                 
+        </section>
+
+    );
+}
+
+export {NavBar, Page};
---
+++ frontend/dev/src/login/Login.js
@@ -0,0 +1,48 @@
+import './Login.css';
+
+function FooterLink(props){
+  return(
+    
+    <a href={props.linkPath} className="text">{props.displayString}</a> 
+           
+  );
+}
+
+function LoginButton(props){
+
+  return(
+
+    <div className = "input-field button">
+          <button className = "test"><i className ={props.iconName} ></i> {props.buttonText}</button>
+    </div>
+    
+  )
+}
+
+
+function Container(){
+  return(
+      <div className='login_main'>
+        <div className="container">
+          <div className="forms">
+            <div className="form login">
+              <form action = "#">
+                <LoginButton
+                  iconName = "fa fa-github fa-lg"  
+                  buttonText = "Login with GitHub"
+                />
+                <FooterLink displayString="Terms of Service" linkPath="#" />
+                <FooterLink displayString="Privacy Policy" linkPath="#" />
+              </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export {FooterLink, Container, LoginButton};
+
+
+
