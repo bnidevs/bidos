072632a926f178bb9734356c6a520a47f7eece1f
Bill Ni <bill.8ni@gmail.com>
2022-11-26 02:07:31 -0500

    add basic project card structure

--- frontend/dev/src/projects/Projects.js
+++ frontend/dev/src/projects/Projects.js
@@ -1,5 +1,6 @@
 import './Projects.css';
 import logo from '../static/logo.png';
+import { useEffect } from 'react';
 
 //links on nav bar
 function NavBar(props){
@@ -12,6 +13,60 @@ function NavBar(props){
 function NavButton(props){
     return(
         <li><a href={props.linkPath} className="login">{props.displayString}</a></li>
+    );
+}
+
+function Switcher(){
+    const logged_in = false;
+
+    let check_logged_in = (event) => {
+        if(!logged_in){
+            console.log("Only verified users can view private projects.");
+            event.preventDefault();
+            return;
+        }
+    };
+
+    return(
+        <span className="flex">
+            <input id="public_switcher" className="switcher left_switcher no_margin" type="radio" name="vis" checked></input>
+            <p className="text">Public</p>
+            <input id="private_switcher" className="switcher right_switcher no_margin" type="radio" name="vis" onClick={check_logged_in}></input>
+            <p className="text">Private &#128274;</p>
+        </span>
+    );
+}
+
+function ProjectsList(){
+    return(
+        <ProjectCard />
+    );
+}
+
+function ProjectCardButton(props){
+    return(
+        <button className="project_card_btn">
+            <h4 className="fonted">{props.text}</h4>
+        </button>
+    );
+}
+
+function ProjectCard(){
+    return(
+        <div className="project_card">
+            <div className="flex spread_out">
+                <h2>bidOS</h2>
+                <h4 className="project_pool">Pool: $12345.67</h4>
+            </div>
+            <div>
+                <p>Crowdfunding open source</p>
+            </div>
+            <div className="spacer"></div>
+            <div className="card_footer flex spread_out">
+                <ProjectCardButton text="Details" />
+                <ProjectCardButton text="Fund" />
+            </div>
+        </div>
     );
 }
 
@@ -29,7 +84,8 @@ function ProjectsPage(props){
             </ul>
         </header>   
         <div className="content">
-            
+            <Switcher />
+            <ProjectsList />
         </div>                 
     </section>
   );
