b5ae3b1a02f5edcf13cd07389e53e0105e28d207
Bill Ni <bill.8ni@gmail.com>
2022-11-29 23:40:55 -0500

    move files into correct locations

--- frontend/dev/src/contact/Contact.js
+++ frontend/dev/src/contact/Contact.js
@@ -18,6 +18,24 @@ function NavBar(props){
       );
   }
 
+  function TextField(props){
+      return(
+        <form>
+            <input type="text" placeholder = {props.displayString} />
+        </form>
+      );
+  }
+
+  function TextButton(props){
+    return(
+      <form>
+          <ul>
+            <li><a href={props.linkPath} className="submission">{props.displayString}</a></li>
+          </ul>
+      </form>
+    );
+}
+
 function ContactPage(){
     return(
         <section className='contact_main'>
@@ -33,7 +51,17 @@ function ContactPage(){
                 
                 </ul>
             </header>  
-            <p>Contact Page</p>
+        
+            <div className="contact_box">
+
+            <form className = "field">
+                    <TextField displayString = "Name"/>
+                    <TextField displayString = "Email"/>
+                    <TextField displayString = "Message"/>
+                    <TextButton displayString = "Submit"/>
+                </form>
+            </div>
+
         </section>
 
     );
