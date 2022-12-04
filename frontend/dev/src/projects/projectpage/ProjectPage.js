5556cf213455a8ebed8834727c68562014832c49
Bill Ni <bill.8ni@gmail.com>
2022-12-04 18:19:50 -0500

    add subrouting
    Co-Authored-By: Abir Taheer <40152590+Abir-Taheer@users.noreply.github.com>

--- frontend/dev/src/projects/projectpage/ProjectPage.js
+++ frontend/dev/src/projects/projectpage/ProjectPage.js
@@ -1,3 +1,16 @@
 import './ProjectPage.css';
-import logo from '../static/logo.png';
+import { PageHeader } from '../../components/Parts';
 import { useEffect, useState } from 'react';
+
+function ProjectPage(props){
+    return(
+      <section className='projects_main'>
+          <PageHeader />
+          <div className="content">
+            test {props.projectId}
+          </div>                 
+      </section>
+    );
+}
+
+export {ProjectPage}
