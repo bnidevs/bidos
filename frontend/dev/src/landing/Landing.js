9b228cfa76f108a98def45d44bfebec3918eb46b
Isaac Efrosman <isaacefrosman3@gmail.com>
2022-12-05 21:38:50 -0500

    Added text on landing page

--- frontend/dev/src/landing/Landing.js
+++ frontend/dev/src/landing/Landing.js
@@ -1,19 +1,50 @@
 import './Landing.css';
-import {PageHeader} from '../components/Parts';
+import logo from '../static/logo.png';
+
+//links on nav bar
+function NavBar(props){
+    return(
+        <li><a href={props.linkPath} className="text">{props.displayString}</a></li>      
+    );
+}
+
+//Button on nav bar 
+function NavButton(props){
+    return(
+        <li><a href={props.linkPath} className="login">{props.displayString}</a></li>
+    );
+}
+
+
+ //<h2 className = "featured">Featured Projects</h2>         
 
 function LandingPage(){
     return(
         <section className='landing_main'>
-            <PageHeader />
+            <header>
+            <a href="/" ><img src={logo} className="logo" alt="logo"/></a>
+                <ul>
+                
+                    <NavBar displayString="Home" linkPath="/"/>
+                    <NavBar displayString="Projects" linkPath="/projects"/>
+                    <NavBar displayString="About" linkPath="/about"/>
+                    <NavBar displayString="Contact" linkPath="/contact"/>
+                    <NavButton displayString="Login" linkPath="/login"/>
+                
+                </ul>
+            </header>   
             <div className="content">
                 <div className="textBox">
                     <h2>Welcome to <span>bidOS</span></h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    
+                    <p className = "blurb">
+                    Open source is free. Always has been. What if we could change that? What if we could give everyone what they want? designers, investors, developers, power users, prosumers, the average joe. bidOS is paving the way for people with ideas to get the resources they need to bring them to life.
                     </p>
                 </div>
-            </div>                 
+            </div>        
+           
         </section>
     );
 }
 
-export {LandingPage};
+export {NavBar, LandingPage};
