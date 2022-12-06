b161cb2d3c398bba522b6e28952e7bca84371df4
Bill Ni <bill.8ni@gmail.com>
2022-12-05 22:50:16 -0500

    fix pr

--- frontend/dev/src/landing/Landing.js
+++ frontend/dev/src/landing/Landing.js
@@ -1,50 +1,20 @@
 import './Landing.css';
-import logo from '../static/logo.png';
-
-//links on nav bar
-function NavBar(props){
-    return(
-        <li><a href={props.linkPath} className="text">{props.displayString}</a></li>      
-    );
-}
-
-//Button on nav bar 
-function NavButton(props){
-    return(
-        <li><a href={props.linkPath} className="login">{props.displayString}</a></li>
-    );
-}
-
-
- //<h2 className = "featured">Featured Projects</h2>         
+import {PageHeader} from '../components/Parts';
 
 function LandingPage(){
     return(
         <section className='landing_main'>
-            <header>
-            <a href="/" ><img src={logo} className="logo" alt="logo"/></a>
-                <ul>
-                
-                    <NavBar displayString="Home" linkPath="/"/>
-                    <NavBar displayString="Projects" linkPath="/projects"/>
-                    <NavBar displayString="About" linkPath="/about"/>
-                    <NavBar displayString="Contact" linkPath="/contact"/>
-                    <NavButton displayString="Login" linkPath="/login"/>
-                
-                </ul>
-            </header>   
+            <PageHeader />
             <div className="content">
                 <div className="textBox">
                     <h2>Welcome to <span>bidOS</span></h2>
-                    
                     <p className = "blurb">
                     Open source is free. Always has been. What if we could change that? What if we could give everyone what they want? designers, investors, developers, power users, prosumers, the average joe. bidOS is paving the way for people with ideas to get the resources they need to bring them to life.
                     </p>
                 </div>
-            </div>        
-           
+            </div>                 
         </section>
     );
 }
 
-export {NavBar, LandingPage};
+export {LandingPage};
