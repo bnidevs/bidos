52fd2e240885fb11e724fe715e0e6631838ef70d
RonglinC <71185648+ronlc@users.noreply.github.com>
2023-02-10 16:57:47 -0500

    test file added
    create files to test add database.

---
+++ backend/routes/put.js
@@ -0,0 +1,24 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'us-east-1'});
+
+// Create the DynamoDB service object
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+var params = {
+  TableName: 'Fund',
+  Item: {
+    'project' : {N: '001'},
+    'project name ' : {S: 'hello'}
+  }
+};
+
+// Call DynamoDB to add the item to the table
+ddb.putItem(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data);
+  }
+});
