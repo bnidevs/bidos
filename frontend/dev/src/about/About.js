80338e426877960b1e91f952dd6ef87aa5c65c4c
Isaac Efrosman <isaacefrosman3@gmail.com>
2023-02-17 16:37:12 -0500

    Created frame of about page
    Added temporary text copied from the landing page

--- frontend/dev/src/about/About.js
+++ frontend/dev/src/about/About.js
@@ -3,10 +3,17 @@ import {PageHeader} from '../components/Parts';
 
 function AboutPage(){
     return(
-        <section className='about_main'>
-            <PageHeader /> 
-            <p>About Page</p>
-        </section>
+       
+    <div className="about-section">
+        <PageHeader />
+       
+        <h1>About Us Page</h1>
+        <p>
+            Open source is free. Always has been. What if we could change that? What if we could give everyone what they want? designers, investors, developers, power users, prosumers, t
+            he average joe. bidOS is paving the way for people with ideas to get the resources they need to bring them to life.
+        </p>
+     
+      </div>
 
     );
 }
