3e70e794bc19e52d5dda0310a1c1ffe3b3b43326
Isaac Efrosman <isaacefrosman3@gmail.com>
2023-02-28 16:25:43 -0500

    Adjusted text blurb and added path to contact page

--- frontend/dev/src/about/About.js
+++ frontend/dev/src/about/About.js
@@ -1,17 +1,32 @@
 import './About.css';
 import {PageHeader} from '../components/Parts';
 
+function ContactButton(props){
+    return(
+        <a href={props.linkPath} className="TestBut">{props.displayString}</a>
+    );
+}
+
+
 function AboutPage(){
     return(
        
     <div className="about-section">
         <PageHeader />
        
-        <h1>About Us Page</h1>
+        <h1>About us</h1>
         <p>
             Open source is free. Always has been. What if we could change that? What if we could give everyone what they want? designers, investors, developers, power users, prosumers, t
             he average joe. bidOS is paving the way for people with ideas to get the resources they need to bring them to life.
         </p>
+
+       <br></br>
+       <br></br>
+        <ContactButton displayString="Contact Us" linkPath="/contact"/>
+
+
+
+
      
       </div>
 
