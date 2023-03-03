e02d43b272b4bf19199af9d8aa3011d71c509b0b
Isaac Efrosman <isaacefrosman3@gmail.com>
2023-03-03 16:18:57 -0500

    Added text blurbs on about page

--- frontend/dev/src/about/About.js
+++ frontend/dev/src/about/About.js
@@ -14,16 +14,21 @@ function AboutPage(){
     <div className="about-section">
         <PageHeader />
        
-        <h1>About us</h1>
-        <p>
-            Open source is free. Always has been. What if we could change that? What if we could give everyone what they want? designers, investors, developers, power users, prosumers, t
-            he average joe. bidOS is paving the way for people with ideas to get the resources they need to bring them to life.
+        <h1 className = "title">⎯ Our goal ⎯</h1>
+        
+        <p className = "blurb">
+            Open source is unpaid. Always has been. What if we could change that? What if we could give everyone what they want? Designers, investors, developers, power users, prosumers, the
+            average joe. bidOS is paving the way for people with ideas to get the resources they need to bring them to life. 
         </p>
-
-       <br></br>
-       <br></br>
-        <ContactButton displayString="Contact Us" linkPath="/contact"/>
-
+        <br></br>
+        <br></br>
+        <h1 className = "title" >⎯ Who we are ⎯</h1>
+        <p  className = "blurb">
+            bidOS is an open source project which started in fall of 2022. The idea began when we realized that it is
+            extremely difficult for people who participate in the open source community to make an income. We believe that the developers working on these projects deserve
+            a way to gain monetary rewards for their valuable contributions and by doing so hopefully attract more attention to the very large field of open source software development.
+        </p>
+       
 
 
 
