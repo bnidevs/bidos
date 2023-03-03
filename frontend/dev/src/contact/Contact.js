773399af657640f9f06a88c2666dcbe75f972f80
Isaac Efrosman <isaacefrosman3@gmail.com>
2023-03-03 16:57:36 -0500

    Created basic contact form

--- frontend/dev/src/contact/Contact.js
+++ frontend/dev/src/contact/Contact.js
@@ -1,41 +1,22 @@
 import './Contact.css';
 import {PageHeader} from '../components/Parts';
 
-function TextField(props){
-      return(
-        <form>
-            <input type="text" placeholder = {props.displayString} />
-        </form>
-      );
-  }
-
-  function TextButton(props){
-    return(
-      <form>
-          <ul>
-            <li><a href={props.linkPath} className="submission">{props.displayString}</a></li>
-          </ul>
-      </form>
-    );
-}
-
 function ContactPage(){
     return(
-        <section className='contact_main'>
-          <PageHeader />
-          
-          <div className="contact_box">
+        <div className = "container">
+          <PageHeader /> 
+          <form> 
+         
 
-            <form className = "field">
-                    <TextField displayString = "Name"/>
-                    <TextField displayString = "Email"/>
-                    <TextField displayString = "Message"/>
-                    <TextButton displayString = "Submit"/>
-                </form>
-            </div>
+          <input name="name" type="text" className="feedback-input" placeholder="Name" />   
+          <input name="email" type="text" className="feedback-input" placeholder="Email" />
+          <input name="text" className="feedback-input" placeholder="Response"/>
+          <input type="submit" value="SUBMIT"/>
+   
+          </form>
+        </div>
 
-        </section>
-
+     
     );
 }
 
