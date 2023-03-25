bd7fc5e668abc289d6c562ad445a815b89c442a7
Isaac Efrosman <isaacefrosman3@gmail.com>
2023-03-25 13:06:32 -0400

    Reformatted page to be right justified and adjusted colors

--- frontend/dev/src/contact/Contact.js
+++ frontend/dev/src/contact/Contact.js
@@ -5,12 +5,16 @@ function ContactPage() {
   return (
     <div className="page-container">
       <PageHeader />
-      <div className="form-container">
+     
         <form>
           <textarea name="text" className="feedback-input resizable" placeholder="Response"></textarea>
           <input type="submit" value="SUBMIT" />
         </form>
-      </div>
+      
+      <p className="cBlurb">
+        Thank you for considering to contact us. If you have an issue with our product or service, the best way to address it is by opening an issue on our GitHub repository. This way, our team can quickly 
+        and efficiently address your concerns and provide you with the support you need. We appreciate your feedback and look forward to hearing from you!
+      </p>
     </div>
   );
 }
