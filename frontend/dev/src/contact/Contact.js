779844325f0ac65011c0cf0bb047e54694a73c31
Isaac Efrosman <isaacefrosman3@gmail.com>
2023-03-25 13:28:16 -0400

    Added hyperlink to repo

--- frontend/dev/src/contact/Contact.js
+++ frontend/dev/src/contact/Contact.js
@@ -12,7 +12,7 @@ function ContactPage() {
         </form>
       
       <p className="cBlurb">
-        Thank you for considering to contact us. If you have an issue with our product or service, the best way to address it is by opening an issue on our GitHub repository. This way, our team can quickly 
+        Thank you for considering to contact us. If you have an issue with our product or service, the best way to address it is by opening an issue on our <a href = "https://github.com/bnidevs/bidos" className = "links">GitHub repository</a>. This way, our team can quickly 
         and efficiently address your concerns and provide you with the support you need. We appreciate your feedback and look forward to hearing from you!
       </p>
     </div>
