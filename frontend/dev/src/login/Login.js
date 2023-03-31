c55e157a9fd8eb147bb424d3b628d7f47a8a88a7
ifoster01 <ifoster41901@gmail.com>
2023-03-31 17:26:06 -0400

    Began adding login with Github
    doesn't work yet and I'm just trying to get it to work on my local computer before using our actual backend

--- frontend/dev/src/login/Login.js
+++ frontend/dev/src/login/Login.js
@@ -1,5 +1,8 @@
 import './Login.css';
 import logo from '../static/logo.png';
+import { useEffect, useState } from 'react';
+
+const CLIENT_ID =  "432bd0957cc93ae4fd86";
 
 function FooterLink(props){
   return(
@@ -10,36 +13,77 @@ function FooterLink(props){
 }
 
 function LoginButton(props){
+  function loginWithGithub() {
+    window.location.assign("https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID);
+  }
 
   return(
 
     <div className = "input-field button">
-          <button className = "test"><i className ={props.iconName} ></i> {props.buttonText}</button>
+          <button className = "test" onClick={loginWithGithub}><i className ={props.iconName}></i> {props.buttonText}</button>
     </div>
     
   )
 }
 
 function LoginPage(){
+  const [loginSuccess, setLoginSuccess] = useState(false);
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const codeParam = urlParams.get("code");
+    console.log(codeParam);
+    
+    if (codeParam !== null) {
+      setLoginSuccess(true);
+    }
+  }, [])
+  
   return(
-      <div className='login_main'>
-        <header>
-        <a href="/" ><img src={logo} className="logo" alt="logo"/></a>
-        </header>
-        <div className="container">
-          <div className="forms">
-            <div className="form login">
-              <form action = "#">
-                <LoginButton
-                  iconName = "fa fa-github fa-lg"  
-                  buttonText = "Login with GitHub"
-                />
-                <FooterLink displayString="Terms of Service" linkPath="#" />
-                <FooterLink displayString="Privacy Policy" linkPath="#" />
-              </form>
+    <div>
+      {loginSuccess === true ? (
+        <div className='login_main'>
+          <header>
+              <a href="/" ><img src={logo} className="logo" alt="logo"/></a>
+          </header>
+          <div className="container">
+              <div className="forms">
+                  <div className="form login">
+                      <div className = "success-text">
+                          <div className = "test">
+                              LOGIN SUCCESS
+                          </div>
+                      </div>
+                      <FooterLink displayString="Terms of Service" />
+                      <FooterLink displayString="Privacy Policy" />
+                      {/* {localStorage.getItem("accessToken") ?
+                          <>
+                          </>
+                          :
+                          <>
+                          </>
+                      } */}
+                  </div>
+              </div>
+          </div>
+      </div>
+      ) : (
+        <div className='login_main'>
+          <header>
+          <a href="/" ><img src={logo} className="logo" alt="logo"/></a>
+          </header>
+          <div className="container">
+            <div className="forms">
+              <div className="form login">
+                  <LoginButton iconName = "fa fa-github fa-lg" buttonText = "Login with GitHub" />
+                  <FooterLink displayString="Terms of Service" linkPath="#" />
+                  <FooterLink displayString="Privacy Policy" linkPath="#" />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
---
+++ frontend/server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var cors = require('cors');
+const fetch = (...args) =>
+    import('node.fetch').then(({default: fetch}) => fetch(...args));
+var bodyParser = require('body-parser');
+
+const CLIENT_ID = "432bd0957cc93ae4fd86";
+const CLIENT_SECRET = "c6f3303fd7e7f7cb6c9d6f94d21aff5a73d11715";
+
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// code being passed from the frontend
+app.get('/getAccessToken', async function (req, res) {
+    req.query.code;
+
+    const params = "?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&code=" + req.query.code;
+    await fetch("https://github.com/login/oauth/access_token" + params, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json"
+        }
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+        res.json(data);
+    });
+});
+
+// getUserData
+// access token is going to be passed in as an Authorization header
+
+app.get('/getUserData', async function (req, res) {
+    req.get("Authorization"); // Bearer ACCESSTOKEN
+    await fetch("https://api.github.com/user", {
+        method: "GET",
+        headers: {
+            "Authorization" : req.get("Authorization")
+        }
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+        res.json(data);
+    })
+})
+
+app.listen(4000, function () {
+    console.log("CORS server running on port 4000");
+})
